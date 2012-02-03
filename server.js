8ee526c665c68302baf553e673e969bb7bffd641
Laurent Couvidou <lorancou@free.fr>
2012-02-03 09:48:39 +0100

    Added mysql nodejs module

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var http = require("http");
 var https = require("https");
 var app = require("express").createServer();
 var io = require("socket.io").listen(app);
+var mysql = require("mysql");
 
 // configure socket.io for production
 io.configure("production", function(){
