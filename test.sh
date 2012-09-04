441bfbc6d135003aaec6adca4b0974cfcb2703b2
Laurent Couvidou <lorancou@free.fr>
2012-09-04 20:05:17 +0200

    Simple test sync script

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# rsync to server
+rsync -avzh --exclude-from=.rsyncignore ./ stachserver:/opt/snakedemocracy.test
+
+# build on server
+#ssh stachserver "cd /opt/snakedemocracy.test && ./build.sh"
+
+# copy www build to Apache server directory
+#ssh stachserver "cd /opt/snakedemocracy.test && rsync -avzh build/stachserver/ /var/www/snakedemocracy"
+
+# bring build back
+#rsync -avzh --delete stachserver:/opt/snakedemocracy.test/build .
+
+# TODO set node env + (re)start server
